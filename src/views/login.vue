80b395465e1979acd25d16a3836a40c085b5920e
粉猪佩奇 <1114310086@qq.com>
2019-08-27 19:48:19 +0800

    搭建完成路由测试模块

---
+++ src/views/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="login">
+      {{ msg }}
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: "login",
+    data() {
+      return {
+        msg: "我是login组件"
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+  .container {
+    width: 100v;
+    height: 100h;
+    background-color: skyblue;
+  }
+</style>
